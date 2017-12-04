0b15f4246244199c8c73b407437d818e8b766ae5
Leif Hetland <lehetland1@gmail.com>
2017-12-04 16:13:53 -0600

    multiple children being created at half second intervals

--- fractals/Assets/Fractal.cs
+++ fractals/Assets/Fractal.cs
@@ -5,23 +5,37 @@ public class Fractal : MonoBehaviour {
 	public Mesh mesh;
 	public Material material;
 	public int maxDepth;
-	public int depth;
+	private int depth;
+	public float childScale;
 
 	private void Start() {
 		gameObject.AddComponent<MeshFilter>().mesh = mesh;
 		gameObject.AddComponent<MeshRenderer>().material = material;
 
 		if (depth < maxDepth) {
-			new GameObject ("Fractal Child").AddComponent<Fractal>().Initialize(this);	
+			StartCoroutine(CreateChildren());
 		}
 		 
 	}
 
-	private void Initialize(Fractal parent) {
+	private IEnumerator CreateChildren() {
+		yield return new WaitForSeconds(0.5f);
+		new GameObject ("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up, Quaternion.identity);
+		yield return new WaitForSeconds(0.5f);
+		new GameObject ("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+		yield return new WaitForSeconds(0.5f);
+		new GameObject ("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+	}
+
+	private void Initialize(Fractal parent, Vector3 direction, Quaternion orientation) {
 		mesh = parent.mesh;
 		material = parent.material;
 		maxDepth = parent.maxDepth;
 		depth = parent.depth + 1;
+		childScale = parent.childScale;
 		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+		transform.localRotation = orientation;
 	}
 }
