9329d441dbd52a4f67d7ed7a06d9dbdd5be48983
Leif Hetland <lehetland1@gmail.com>
2017-12-04 14:11:46 -0600

    creating cubes with children programmatically

---
+++ fractals/Assets/Fractal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public int depth;
+
+	private void Start() {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+
+		if (depth < maxDepth) {
+			new GameObject ("Fractal Child").AddComponent<Fractal>().Initialize(this);	
+		}
+		 
+	}
+
+	private void Initialize(Fractal parent) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		transform.parent = parent.transform;
+	}
+}
